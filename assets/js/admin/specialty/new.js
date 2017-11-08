832115a4f17543de580c2de7eaf2e5f2f856b98c
Aiman Asyraf Bin Anuar <imbacakes@gmail.com>
2017-11-09 00:04:56 +0800

    added js logic to render new input for form data

---
+++ assets/js/admin/specialty/new.js
@@ -0,0 +1,12 @@
+const addFormData = document.getElementById("add-form-data-field")
+
+addFormData.addEventListener("click", function(e){
+  e.preventDefault()
+  const containerFormData = document.getElementById("form-data-container")
+  const numberOfField = containerFormData.childElementCount
+  const newField = document.createElement("input")
+  newField.setAttribute("type", "text")
+  newField.setAttribute("name", "specialty[form-data]")
+  newField.setAttribute("id", "form-field-" + numberOfField)
+  containerFormData.append(newField)
+})
