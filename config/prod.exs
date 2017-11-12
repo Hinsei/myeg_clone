5d6ec8a2889d7dfa2826f28ed511b18b24331e17
Aiman Asyraf Bin Anuar <imbacakes@gmail.com>
2017-11-12 17:29:04 +0800

    error in prod config

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :myeg, MyegWeb.Endpoint,
 
 config :myeg, Myeg.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env('DATABASE_URL'),
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
